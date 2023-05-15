abca890ed5799950f716c17b496f25e94087ff7f
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-05-15 17:32:16 +0900

    00_03　ウィンドウの生成

---
+++ WinApp.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+#include <cstdint>
+
+class WinApp
+{
+public:
+	//ウィンドウサイズ
+	static const int32_t kClientWidth = 1280;//横幅
+	static const int32_t kClientHeight = 720;//縦幅
+
+	//ウィンドウプロシージャ
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+	static void CreateWindowView(const wchar_t* title = L"CG2");
+
+private:
+	// ウィンドウクラス
+	static inline WNDCLASS wc{};
+
+	//ウィンドウオブジェクト
+	static HWND hwnd;
+};
+
